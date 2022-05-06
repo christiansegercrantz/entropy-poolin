f231b8ea65acae221e8bc3cecf27a4a613e01d5d
Christian Segercrantz <segercc1@aalto.fi>
2022-05-06 12:17:59 +0300

    Bug fixes, changed optimizion space for markov

--- entropy_pooling/markoviz_optimizer.py
+++ entropy_pooling/markoviz_optimizer.py
@@ -1,10 +1,10 @@
-from turtle import color
 import numpy as np
 import pandas as pd
 from scipy.optimize import minimize, LinearConstraint, Bounds
 import matplotlib.pyplot as plt
+from plotnine import ggplot, geom_point, aes, geom_line, labs, geom_text, position_jitter, theme, element_text, theme_linedraw, element_line, element_rect
 
-def load_asset_deltas(filename, sheet_name = None):
+def load_asset_deltas(filename, sheet_name = 0):
     """Uploads the data containing the asset delta matrix from the given Excel file.
     --------------------
     ### Input arguments:
@@ -22,15 +22,16 @@ def load_asset_deltas(filename, sheet_name = None):
     """
 
     deltas = pd.read_excel(filename, sheet_name, header = 0, index_col = 0)
+
     # If there are any missing values, convert them to zeros
     deltas = deltas.fillna(0)
     asset_names = list(deltas.index)
     # Convert delta matrix to a simple numpy array
-    deltas = deltas.to_numpy
+    deltas = deltas.to_numpy()
 
     return deltas, asset_names
 
-def asset_scenarios(factor_scenarios, asset_deltas):
+def asset_scenarios(factor_scenarios, asset_deltas, asset_names):
     """Computes the scenario-wise returns for each portfolio asset using the factor scenarios matrix and asset delta matrix.
     --------------------
     ### Input arguments:
@@ -38,6 +39,8 @@ def asset_scenarios(factor_scenarios, asset_deltas):
         A (S x F) matrix of factor return scenarios
         asset_deltas: Matrix
         A (N x F) matrix of asset sensitivities ('deltas') to changes in factors
+        asset_names: List
+        A (N x 1) list of the asset names
     --------------------
     ### Returns:
         asset_scenarios:
@@ -47,9 +50,10 @@ def asset_scenarios(factor_scenarios, asset_deltas):
     # Check that the F dimension matches
     assert factor_scenarios.shape[1] == asset_deltas.shape[1], "The number of factors (x dimension) is not the same for the input matrices."
     asset_scenarios = factor_scenarios @ asset_deltas.T
+    asset_scenarios.columns = asset_names
     return asset_scenarios
 
-def optimizer(scenarios, probabilities, mu_0, disp = True, visualize = False):
+def optimizer(scenarios, probabilities, mu_0, visualize = False, verbose = 0):
     """Optimizes the weights put on each item the portfolio. This is done by minimizing the volatility of the portfolio at a given return procentage. Also visualized the markoviz model if requested.
     --------------------
     ### Input arguments:
@@ -59,10 +63,10 @@ def optimizer(scenarios, probabilities, mu_0, disp = True, visualize = False):
             A (S x 1) vector of prior probabilities
         mu_0: Float
             The return to optimize for, given in decimal as 50% = 0.5
-        disp: Default 'True'
-            the (K x S) matrix used to express the constraints Cx <= d
-        visualize: Default 'False'
+        visualize: Boolean, Default 'False'
             Plots the efficient prontier, the optimal protoflio, the original portfolio items and a cloud of randomly weighted items.
+        verbose: {0,1,2} , Default '0'
+            Weather to display extra information about the optimization. 0 will display nothing, 1 will display the result of the optimiztion and and error message in case it was not succesful, 2 will additionally to 1 display convergence messages of the optimizer. 
     --------------------
     ### Returns:
         res:
@@ -71,8 +75,9 @@ def optimizer(scenarios, probabilities, mu_0, disp = True, visualize = False):
 
     #In case the probabilities vector  is of shape (n,) instead of (n,1)
     if probabilities.ndim == 1:
-        probabilities = probabilities.reshape(len(probabilities),1) #try -1 instead  of len(...)
+        probabilities = probabilities.reshape(-1,1)
 
+    #Get the mean vector and covariance matrix
     mu, covar = mean_and_var(scenarios, probabilities)
 
     m, n = covar.shape
@@ -86,9 +91,11 @@ def optimizer(scenarios, probabilities, mu_0, disp = True, visualize = False):
 
     bounds = Bounds(lb = np.zeros(m), ub = np.ones(m)) #[(0, 1) for i in range(m)]
     constraints = (LinearConstraint(np.ones(m), lb=1, ub=1), #Sum of weights 1
-                  LinearConstraint(mu, lb=mu_0, ub=np.inf) #Greater or equal to a certain return level
+                   LinearConstraint(mu, lb=mu_0, ub=np.inf) #Greater or equal to a certain return level
                   )
 
+    disp = True if verbose == 2 else False 
+    
     res = minimize(objective_function,
                    jac=jac,
                    x0 = x0,
@@ -96,6 +103,11 @@ def optimizer(scenarios, probabilities, mu_0, disp = True, visualize = False):
                    constraints = constraints,
                    tol=0.00001,
                    options = {"disp": disp})
+    if verbose:
+        print(f"The optimization was succesful: {res.success}")
+        if not res.success:
+            print(f"The optimization was terminated due to: \n{res.message}")
+    
     if visualize:
         vizualization(covar, mu, optimal = res.x, frontier=True, scenarios = scenarios, probabilities = probabilities)
     return res
@@ -123,7 +135,14 @@ def mean_and_var(scenarios, probabilities):
     covar = np.cov(scenarios, rowvar = False, aweights = probabilities_reshaped) #* 252 #annualization constant
     return mu, covar
 
-def vizualization(covar, mu, generated_points = 50000, frontier = True, optimal = None, scenarios = None, probabilities = None, mu_0 = None):
+def vizualization(covar,
+                  mu,
+                  generated_points = 50000,
+                  frontier = True,
+                  optimal = None,
+                  scenarios = None,
+                  probabilities = None,
+                  mu_0 = None):
     """ Visualizes the markoviz model, the original portfolio items and the optimal points. Optimally calculates the optimal results and plots the efficient frontier
     --------------------
     ### Input arguments:
@@ -165,7 +184,7 @@ def vizualization(covar, mu, generated_points = 50000, frontier = True, optimal 
         frontier_mu = np.array([])
         frontier_var = np.array([])
         for j in np.linspace(0, np.max(mu), 100):
-            opt = optimizer(scenarios, probabilities, mu_0 = j, disp = False)
+            opt = optimizer(scenarios, probabilities, mu_0 = j, verbose = 0)
             frontier_mu = np.append(frontier_mu, mu @ opt.x)
             frontier_var = np.append( frontier_var, opt.x.T @ covar @ opt.x)
 
@@ -180,7 +199,7 @@ def vizualization(covar, mu, generated_points = 50000, frontier = True, optimal 
     #     ax.plot(frontier_var, frontier_mu, color='red');
     # plt.show();
 
-    from plotnine import ggplot, geom_point, aes, geom_line, labs, geom_text, position_jitter, theme, element_text, theme_linedraw, element_line, element_rect
+    
 
     generated_df = pd.DataFrame(list(zip(port_vol,
                                     port_returns)),
@@ -193,6 +212,18 @@ def vizualization(covar, mu, generated_points = 50000, frontier = True, optimal 
     #df = pd.DataFrame(list(zip(frontier_var,frontier_mu)),
     #                    columns = ["Volatility", "Returns"])
     (ggplot()
+        + theme(legend_title = element_text(
+                family = "Calibri",
+                colour = "brown",
+                face = "bold",
+                size = 12))
+        + theme(panel_grid_major = element_line(size = 0.5,
+                                                linetype = 'solid',
+                                                colour = "black"),
+                panel_grid_minor = element_line(size = 0.25,
+                                                linetype = 'solid',
+                                                colour = "black"),
+                panel_background = element_rect(fill = "white"))
         + labs(title="Optimal solution", y="Returns", x="Volatility")
         + geom_point(data = generated_df,
                      mapping = aes(x = "Volatility",
@@ -210,8 +241,8 @@ def vizualization(covar, mu, generated_points = 50000, frontier = True, optimal 
                      )
         + geom_text(mapping = aes(x = optimal.T @ covar @ optimal, y=mu @ optimal),
                      label = "Optimal",
-                     nudge_y = -0.05,
-                     nudge_x = 5,
+                     nudge_y = -0.02,
+                     nudge_x = 3,
                      size = 7,
                      )
         + geom_point(aes(x = np.diag(covar), y = mu),
@@ -219,21 +250,10 @@ def vizualization(covar, mu, generated_points = 50000, frontier = True, optimal 
                      )
         + geom_text(aes(x = np.diag(covar), y = mu),
                      label = scenarios.columns,
-                     nudge_y = -0.05,
-                     nudge_x = 5,
+                     nudge_y = -0.02,
+                     nudge_x = 3,
                      size = 7,
                      position=position_jitter()
                      )
-        + theme(legend_title = element_text(
-                family = "Calibri",
-                colour = "brown",
-                face = "bold",
-                size = 12))
-        + theme(panel_grid_major = element_line(size = 0.5,
-                                                linetype = 'solid',
-                                                colour = "black"),
-                panel_grid_minor = element_line(size = 0.25,
-                                                linetype = 'solid',
-                                                colour = "black"),
-                panel_background = element_rect(fill = "white"))
+
     ).draw()
